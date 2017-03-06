c4d31557cde26c2e5f63a9effdf0a22ddab88a99
fedyapro <rbk119@gmail.com>
2017-03-06 23:28:22 +0300

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -1,15 +1,15 @@
+#ifndef _MATRIX_H_
+#define _MATRIX_H_
+
 #include <stdio.h>
-#include <malloc.h>
-#include <string.h>
-#include <stdbool.h>
-
+ 
 typedef struct Matrix {
-	int row;
-	int col;
-	double** mat;
-	
+    int row;
+    int col;
+    double** mat;
+   
 } Matrix;
-
+ 
 Matrix* create_matrix(int row, int col);
 void set_elem(Matrix* matrix, int row, int col, double val);
 Matrix* create_matrix_from_file(FILE* file);
@@ -17,7 +17,7 @@ void free_matrix(Matrix* matrix);
 double get_elem(Matrix* matrix, int row, int col);
 int get_rows(Matrix* matrix);
 int get_cols(Matrix* matrix);
-void show_mat(Matrix *matr)
-Matrix* trans_mat(Matrix* matr)
+void show_mat(Matrix *matr);
+Matrix* trans_mat(Matrix* matr);
 
-
+#endif //_MATRIX_H_
