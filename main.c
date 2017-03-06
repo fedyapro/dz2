ea1d12aa9f3e49e3e26dbb712677694a22671d2e
fedyapro <rbk119@gmail.com>
2017-03-06 23:26:06 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,27 +1,31 @@
 #include "matrix.h"
-
+#include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+#include <stdbool.h>
+ 
 int main (int argc, char *argv[]) {
-
-	
-	FILE *f = fopen(argv[1], "r");
-	if (f == NULL){
-		printf ("cant open file\n");
-		return 1;
-	} 
-	
-	
-	Matrix *matrix = create_matrix_from_file(f);
-	if(matrix == NULL)
-		return -1;	
-	
-	Matrix *tmatrix = trans_mat(matrix);
-	if(tmatrix == NULL)
-		return -1;
-	show_mat(tmatrix);
-
-	
-	free_matrix(matrix);
-	free_matrix(tmatrix);	
-	return 0;
+ 
+   
+    FILE *f = fopen(argv[1], "r");
+    if (f == NULL){
+        printf ("cant open file\n");
+        return 1;
+    }
+   
+   
+    Matrix *matrix = create_matrix_from_file(f);
+    if(matrix == NULL)
+        return -1; 
+   
+    Matrix *tmatrix = trans_mat(matrix);
+    if(tmatrix == NULL)
+        return -1;
+    show_mat(tmatrix);
+ 
+   
+    free_matrix(matrix);
+    free_matrix(tmatrix);  
+    return 0;
 }
 
